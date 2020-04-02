f4b35e697d61c458d9dc0d485c312faf2b8d7a9c
asmasoualah <asmasoualah18@gmail.com>
2020-04-02 12:31:12 +0200

    modification de maincomp

--- time/Numl/CompilNum/maincomp.ml
+++ time/Numl/CompilNum/maincomp.ml
@@ -70,61 +70,57 @@ let _ = err ("\nt''="^(printExpr t'')) in
 in t3
 ;;
 
-let _ = 
-  try
-        let _ = symb := 0 ; symbE := 0 in 
-        let lexbuf = Lexing.from_channel stdin in
-        try 
-          let c = Parser.compile Lexer.token lexbuf in
-          let fic = open_out "comp.ml" in                   
-          let fig = open_out "gomp.ml" in
-          let _ = iterCmdList c fic fig in              
-          let _ = close_out fic in 
-          let _ = close_out fig in 
-            
+let rec iterCmdList cl fic fig = match cl with
+                 [] -> ()
+                 | c::cs ->
+             (try (
+              match c with
+                TopExpr(e) ->
+                                  let typeEnv' = copyTypeEnv !typeEnv [] in
+                                  let t = typeCheck e typeEnv' varEnv in
+                                             let t' = evalPart t in
+                                             let s = genCaml e in
+                                             let _ = output_string fic s in
+                                             let k = genGmp e in
+                                             let _ = output_string fig k in
+                                                 iterCmdList cs fic fig
 
-         let rec iterCmdList cl fic fig = match cl with
-                [] -> ()
-                | c::cs -> 
-            (match c with
-		   TopExpr(e) -> 
-		              let typeEnv' = copyTypeEnv !typeEnv [] in
-			      let t = typeCheck e typeEnv' varEnv in	
-                             	    let t' = evalPart t in 
-                                    let s = genCaml e in
-                                    let _ = output_string fic s in
-                                    let k = genGmp e in
-                                    let _ = output_string fig k in
-                                      iterCmdList cs fic fig 
-                              
-             | TopAssign(x,e) -> let typeEnv' = copyTypeEnv !typeEnv [] in
-			         let t = typeCheck e typeEnv' varEnv in
-                                            let t' = evalPart t in
-                                            let s = "let "^x^" = "^(genCaml e) in
-                                            let _ = output_string fic s in
-                                            let k = "let "^x^" = "^(genGmp e) in
-                                            let _ = output_string fig k in
-                                               iterCmdList cs fic fig
+              | TopAssign(x,e) -> let typeEnv' = copyTypeEnv !typeEnv [] in
+                                  let t = typeCheck e typeEnv' varEnv in
+                                             let t' = evalPart t in
+                                             let s = "let "^x^" = "^(genCaml e) in
+                                             let _ = output_string fic s in
+                                             let k = "let "^x^" ="^(genGmp e) in
+                                             let _ = output_string fig k in
+                                                iterCmdList cs fic fig
 
-             | TopRecAssign(x,e) -> let typeEnv' = copyTypeEnv !typeEnv [] in
-                                    let typeEnv'' = typeEnv' in
-                                            let t = recTypeCheck x e typeEnv'' varEnv in 
-                                            let t' = if (isRec t []) then unRec t [] else t in
-                                            let t'' = evalPart t' in
-                                            let s = "let rec"^x^" = "^(genCaml e) in
-                                            let _ = output_string fic s in 
-                                            let k = "let rec"^x^" = "^(genGmp e) in
-                                            let _ = output_string fig k in 
-                                               iterCmdList cs fic fig
-                                               (*| _ -> print_string ("Error: This expression has type "^(printExpr t'')^ 
-                                                            "\n       This is not a function; it cannot be applied."); *)
-                                                                                       
-            )
-        with
+              | TopRecAssign(x,e) -> let typeEnv' = copyTypeEnv !typeEnv[] in
+                                     let typeEnv'' = typeEnv' in
+                                             let t = recTypeCheck x e typeEnv'' varEnv in
+                                             let t' = if (isRec t [])then unRec t [] else t in
+                                             let t'' = evalPart t' in
+                                             let s = "let rec"^x^" ="^(genCaml e) in
+                                             let _ = output_string fic s in
+                                             let k = "let rec"^x^" ="^(genGmp e) in
+                                             let _ = output_string fig k in
+                                                iterCmdList cs fic fig
+                                               
+         ) with
           Failure(s) -> print_string ("Parse errori: "^s)
-        | Parsing.Parse_error -> print_string ("Parse error")
-        | Error(e) -> print_string ("Error: "^e) 
-        | RecTypeError(_,e) -> print_string ("\nError: "^e)
-  with  Error(e) -> print_string ("\nFatal error: "^e^"\n")
-      | RecTypeError(_,e) -> print_string ("\nFatal error: "^e^"\n")
+         | Parsing.Parse_error -> print_string ("Parse error")
+         | Error(e) -> print_string ("Error: "^e)
+         | RecTypeError(_,e) -> print_string ("\nError: "^e)
+         )
 ;;
+
+let _ =
+           let _ = symb := 0 ; symbE := 0 in
+           let lexbuf = Lexing.from_channel stdin in
+           let c = Parseurcomp.compile Lexer.token lexbuf in
+           let fic = open_out "comp.ml" in
+           let fig = open_out "gomp.ml" in
+           let _ = iterCmdList c fic fig in
+           let _ = close_out fic in
+           let _ = close_out fig in () ;;
+
+
